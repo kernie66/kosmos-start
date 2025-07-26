5b07eb64c4e71622f0acb01db0990847329127b3
Kenneth Jagenheim <kenneth.jagenheim@bredband.net>
2025-07-26 13:28:42 +0200

    Refactored PreviewImage with custom hook

--- src/components/upload/PreviewImage.tsx
+++ src/components/upload/PreviewImage.tsx
@@ -1,5 +1,6 @@
 import { Box, Image, Text, UnstyledButton } from '@mantine/core';
-import { memo, useLayoutEffect, useState } from 'react';
+import { memo } from 'react';
+import { useImageParams } from '~/hooks/useImageParams';
 
 export type ImageStateSelections = '100%' | '95vh' | '100vh' | '100vw' | 'auto';
 export type ImageStateProps = ImageStateSelections | number;
@@ -16,31 +17,8 @@ type PreviewImageProps = {
   maxHeight?: ImageStateSelections | number;
 };
 
-const isNumber = (value: ImageStateSelections | number) => typeof value === 'number';
-
 const PreviewImage = memo(({ image, onImageClicked, maxHeight = '100%' }: PreviewImageProps) => {
-  const [imageHeight, setImageHeight] = useState<ImageStateProps>(0);
-  const [imageWidth, setImageWidth] = useState('100%');
-  const [maxImageHeight, setMaxImageHeight] = useState<ImageStateProps>('95vh');
-  const [maxImageWidth, setMaxImageWidth] = useState<ImageStateProps>('100vw');
-
-  useLayoutEffect(() => {
-    console.log('Image size adjusted:', maxHeight);
-
-    const aspectRatio = image.width / image.height;
-    const newMaxImageWidth =
-      isNumber(maxHeight) && isNumber(aspectRatio) ? Math.trunc(maxHeight * aspectRatio) : '100vw';
-    if (aspectRatio > 1) {
-      setImageWidth('100%');
-      setImageHeight('auto');
-      setMaxImageHeight(maxHeight);
-    } else {
-      setImageWidth('auto');
-      setImageHeight(maxHeight);
-      setMaxImageHeight('95vh');
-    }
-    setMaxImageWidth(newMaxImageWidth);
-  }, [image, maxHeight]);
+  const { imageHeight, imageWidth, maxImageHeight, maxImageWidth } = useImageParams({ image, maxHeight });
 
   if (!image.imageUrl || image.width === 0 || image.height === 0) {
     return (
@@ -51,12 +29,6 @@ const PreviewImage = memo(({ image, onImageClicked, maxHeight = '100%' }: Previe
       </Box>
     );
   }
-
-  console.log('fileName', image.fileName);
-  console.log('imageWidth', imageWidth);
-  console.log('imageHeight', imageHeight);
-  console.log('maxImageWidth maxHeight', maxImageWidth, maxHeight);
-  console.log('maxImageHeight', maxImageHeight);
 
   return (
     <UnstyledButton onClick={onImageClicked} h={imageHeight} mah={maxImageHeight} w={imageWidth} maw={maxImageWidth}>
